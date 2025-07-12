989ab1d332b4bcee54a07953f3b2f24999b4285c
mahnaf <mahnaf125@gmail.com>
2025-07-12 16:35:14 +0600

    refactor(auth): complete authentication hook
    Create necessary functions and states in authentication hook

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,9 +1,46 @@
+import { useEffect, useState } from "react";
+import { auth } from "../../config/firebase.config";
+import {
+	createUserWithEmailAndPassword,
+	GoogleAuthProvider,
+	onAuthStateChanged,
+	signInWithEmailAndPassword,
+	signInWithPopup,
+	signOut,
+} from "firebase/auth";
+
 const useAuth = () => {
-	const tempUser = {
-		email: "dev@tournext.com",
+	const [user, setUser] = useState(null);
+	const [loading, setLoading] = useState(true);
+	const signUpWithEmailAndPassword = (email, password) => {
+		return createUserWithEmailAndPassword(auth, email, password);
 	};
+	const loginWithEmailAndPassword = (email, password) => {
+		return signInWithEmailAndPassword(auth, email, password);
+	};
+	const loginWithGoogle = () => {
+		const provider = new GoogleAuthProvider();
+		return signInWithPopup(auth, provider);
+	};
+	const logout = () => {
+		return signOut(auth);
+	};
+	useEffect(() => {
+		const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+			setUser(currentUser);
+			setLoading(false);
+		});
+		return () => {
+			unSubscribe();
+		};
+	}, []);
 	return {
-		tempUser,
+		user,
+		loading,
+		signUpWithEmailAndPassword,
+		loginWithEmailAndPassword,
+		loginWithGoogle,
+		logout,
 	};
 };
 
